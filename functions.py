70cb4aaf89a42607af951b8050f17b2dd625bcb6
rnambaa <ryo.namba@gmail.com>
2023-05-16 18:00:51 +0200

    added comp aggregation function & my own testing file

--- functions.py
+++ functions.py
@@ -7,6 +7,32 @@ def feature_engineering(df):
     df["price_diff"] = (df["price_usd"] - mean_search_price) / mean_search_price
 
 
+def comp_aggregation(df):
+    df_copy = df.copy()
+    # competitor rate variables 
+    comp_rate_vars = ['comp1_rate', 'comp2_rate', 'comp3_rate', 'comp4_rate', 'comp5_rate', 'comp6_rate', 'comp7_rate', 'comp8_rate']
+    df_copy['comp_rate_ratio'] = df_copy[comp_rate_vars].mean(axis=1)
+    # replace NaN with 0 & remove original vars
+    df_copy['comp_rate_ratio'] = df_copy['comp_rate_ratio'].fillna(0)
+    df_copy = df_copy.drop(comp_rate_vars, axis=1)
+
+
+    # competitor availability variables 
+    comp_inv_vars = ['comp1_inv', 'comp2_inv', 'comp3_inv', 'comp4_inv', 'comp5_inv', 'comp6_inv', 'comp7_inv', 'comp8_inv']
+    df_copy['comp_inv_ratio'] = df_copy[comp_inv_vars].mean(axis=1)
+    # replace NaN with 0.5 & remove original vars
+    df_copy['comp_inv_ratio'] = df_copy['comp_rate_ratio'].fillna(0.5)
+    df_copy = df_copy.drop(comp_inv_vars, axis=1)
+
+    # remove competitor rate difference because we said so 
+    comp_rate_diff_vars = ['comp1_rate_percent_diff', 'comp2_rate_percent_diff', 'comp3_rate_percent_diff', 
+                           'comp4_rate_percent_diff', 'comp5_rate_percent_diff', 'comp6_rate_percent_diff', 
+                           'comp7_rate_percent_diff', 'comp8_rate_percent_diff']
+    df_copy = df_copy.drop(comp_rate_diff_vars, axis=1)
+
+    return df_copy
+
+
 def preprocess(df):
     df["relevance"] = df["booking_bool"].apply(lambda x: 5 if x == 1 else 0) + df[
         "click_bool"
