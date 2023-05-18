19d6694599f9de12fb949c67dd58c5996f48733b
NMoolhuijsen <nikki-moolhuijsen@hotmail.com>
2023-05-18 17:43:06 +0200

    missing values

--- functions.py
+++ functions.py
@@ -1,16 +1,49 @@
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
 
 def feature_engineering(df):
     # add a column that indicates the percental difference in price between that row and the average price of search
     mean_search_price = df.groupby("srch_id")["price_usd"].transform("mean")
     df["price_diff"] = (df["price_usd"] - mean_search_price) / mean_search_price
 
-    # # group over srch_id and prop_id and aggregate the comp rates
-    # columns = ['comp1_rate', 'comp2_rate', 'comp3_rate', 'comp4_rate', 'comp5_rate', 'comp6_rate', 'comp7_rate', 'comp8_rate']
-    # df["expedia_lowest"] = df[columns].sum()/8
     
+def remove_variables(df):
+    # variables to remove
+    to_drop = ['visitor_hist_starrating', 'visitor_hist_adr_usd', 'srch_query_affinity_score']
 
+    # remove variables
+    df = df.drop(to_drop, axis=1)
+    return df
+
+def impute_missing_values(df):
+    # predictor and dependent variables
+    dependents = ['orig_destination_distance', 'prop_location_score2', 'prop_review_score']
+    predictors = [['prop_country_id', 'visitor_location_country_id', 'srch_destination_id'], ['prop_location_score1', 'prop_id'], ['prop_starrating', 'prop_id', 'prop_location_score1']]
+
+    # loop over all dependent variables
+    for dependent in dependents:
+        index = dependents.index(dependent)
+        # split data into train and test sets
+        training_set = df.dropna(subset=[dependent])
+        missing_set = df[df[dependent].isnull()]
+
+        # linear regression model
+        regression_model = LinearRegression()
+        regression_model.fit(training_set[predictors[index]], training_set[dependent])
+
+        predicted_values = regression_model.predict(missing_set[predictors[index]])
+
+        # replace missing values with predicted values
+        if dependent == 'prop_review_score':
+            predicted_values = [round(x * 2) / 2 for x in predicted_values.copy()]
+        missing_set[dependent] = predicted_values
+
+        # combine dataframes
+        df = pd.concat([training_set, missing_set]).sort_index()
+    return df
 
 def comp_aggregation(df):
     df_copy = df.copy()
